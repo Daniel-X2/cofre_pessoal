de84d6841d2e8c2511348382e58639f3ad4f0c15
Daniel da silva <foconoaltar123@gmail.com>
2025-09-02 12:41:49 -0300

    feature: criptografia , tela de login , cmake
    adicionei a logica da criptografia falta integrar a aplicaçao, e fiz o esqueleto da tela de login, o cmake esta pronto pra usar no projeto

--- src/main.c
+++ src/main.c
@@ -7,13 +7,13 @@ GtkWidget * botao_nota;
 GtkWidget * lixeira;
 GtkWidget * senhas;
 GtkWidget * input_nome;
-
+GtkWidget * janela_note;
 GtkWidget * teste;
 
 void botao();
 void botao_senhas();
-
-
+void inputs();
+void botoes();
 void botao_lixeira();
 void botao_notas();
 int main(int argc, char *argv[]) {
@@ -30,22 +30,25 @@ int main(int argc, char *argv[]) {
     }
     builder = gtk_builder_new_from_file(caminho);//depois mudar o arquivo .glade pra xml
     free(caminho);
+    
+    janela_note=GTK_WIDGET(gtk_builder_get_object(builder,"gtknotebook"));
     teste=GTK_WIDGET(gtk_builder_get_object(builder,"favorito"));
-    g_signal_connect(teste,"clicked",G_CALLBACK(botao),NULL);
+    g_signal_connect(teste,"clicked",G_CALLBACK(inputs),NULL);
+    
+    
+    botoes();
+    gtk_notebook_set_current_page(GTK_NOTEBOOK(janela_note),0);
 
+    g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
+    //printf("ola mundo");
+    gtk_widget_show_all(window);
+    gtk_main();
     
 
-
-
-
-
-
-
-
-
-
-
-
+    return 1;
+}
+void botoes()
+{
     window = GTK_WIDGET(gtk_builder_get_object(builder, "window1"));
     //note=GTK_WIDGET(gtk_builder_get_object(builder,"ola"));
     botao_nota=GTK_WIDGET(gtk_builder_get_object(builder,"notas"));
@@ -61,40 +64,30 @@ int main(int argc, char *argv[]) {
     
     g_signal_connect(botao_nota,"clicked",G_CALLBACK(botao_notas),NULL);
     
-   
-
-    g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
-    //printf("ola mundo");
-    gtk_widget_show_all(window);
-    gtk_main();
-    
-   
-
-    return 1;
 }
 void botao_notas()
 {
-    GtkWidget * janela_note1;
+    
 
 
-    janela_note1=GTK_WIDGET(gtk_builder_get_object(builder,"gtknotebook"));
+    janela_note=GTK_WIDGET(gtk_builder_get_object(builder,"gtknotebook"));
     //janela_notas=GTK_WIDGET(gtk_builder_get_object(builder,"janela_anotacoes"));
-    gtk_notebook_set_current_page(GTK_NOTEBOOK(janela_note1),0);
+    gtk_notebook_set_current_page(GTK_NOTEBOOK(janela_note),0);
 }
 void botao_lixeira()
 {
-    GtkWidget * janela_note3;
-    janela_note3=GTK_WIDGET(gtk_builder_get_object(builder,"gtknotebook"));
+    
+    janela_note=GTK_WIDGET(gtk_builder_get_object(builder,"gtknotebook"));
 
-    gtk_notebook_set_current_page(GTK_NOTEBOOK(janela_note3),2);
+    gtk_notebook_set_current_page(GTK_NOTEBOOK(janela_note),2);
     
 }
 void botao_senhas()
 {
-    GtkWidget * janela_note2;
-    janela_note2=GTK_WIDGET(gtk_builder_get_object(builder,"gtknotebook"));
+    
+    janela_note=GTK_WIDGET(gtk_builder_get_object(builder,"gtknotebook"));
 
-    gtk_notebook_set_current_page(GTK_NOTEBOOK(janela_note2),1);
+    gtk_notebook_set_current_page(GTK_NOTEBOOK(janela_note),1);
 }
 
 
@@ -106,4 +99,20 @@ void botao()
     const gchar *n1=gtk_entry_get_text(GTK_ENTRY(input_nome));
     printf("%s\n",n1);
 
+}
+void inputs()
+{
+    int n1=gtk_notebook_get_current_page(GTK_NOTEBOOK(janela_note));
+
+    printf("iae %i\n",n1);
+    switch (n1)
+    {
+    case 0:
+        break;
+    case 1:
+        break;
+    case 2:
+        break;
+
+    }
 }
---
+++ teste_criptografia/build/CMakeFiles/meu_app.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/meu_app.dir/link.d"
+  "CMakeFiles/meu_app.dir/main.c.o"
+  "CMakeFiles/meu_app.dir/main.c.o.d"
+  "meu_app"
+  "meu_app.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/meu_app.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
